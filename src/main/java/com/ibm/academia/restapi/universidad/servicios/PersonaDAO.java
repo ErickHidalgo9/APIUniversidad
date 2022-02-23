8bade4b31211b823881547c153c7fdd662724b0b
ErickHidalgo9 <99521248+ErickHidalgo9@users.noreply.github.com>
2022-02-22 22:09:11 -0600

    Update PersonaDAO.java

--- src/main/java/com/ibm/academia/restapi/universidad/servicios/PersonaDAO.java
+++ src/main/java/com/ibm/academia/restapi/universidad/servicios/PersonaDAO.java
@@ -6,12 +6,10 @@ import com.ibm.academia.restapi.universidad.modelo.entidades.Persona;
 
 public interface PersonaDAO extends GenericoDAO<Persona>{
 	
-	//ESTO YA ES CON JPQUERY JPQL
 public Optional<Persona> buscarPorNombreYApellido(String nombre, String apellido);
 
 public Optional<Persona> buscarPorDni(String dni);
 
-//Se refiere a que buscara una lista de personas por el apellido
 public Iterable<Persona> buscarPersonaPorApellido(String apellido);
 
 }
