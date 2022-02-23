05bc924329422612bc073d7c1eaab307072875a6
ErickHidalgo9 <99521248+ErickHidalgo9@users.noreply.github.com>
2022-02-22 22:05:50 -0600

    Update CarreraRepository.java

--- src/main/java/com/ibm/academia/restapi/universidad/repositorios/CarreraRepository.java
+++ src/main/java/com/ibm/academia/restapi/universidad/repositorios/CarreraRepository.java
@@ -9,19 +9,10 @@ import com.ibm.academia.restapi.universidad.modelo.entidades.Carrera;
 @Repository
 public interface CarreraRepository extends CrudRepository<Carrera, Long>{
 	
-	//Buscar las carreras que contienen algun nombre
-	//@Query ("select c from Carrera c where c.nombre like %?1%") esto es igual al metodo de abajo que ya lo hace
 	public Iterable<Carrera> findCarrerasByNombreContains(String nombre);
-	
-	//Ignorecase ignora mayusculas y minusculas
-	//@Query ("select c from Carrera c where upper(c.nombre) like upper(%?1%")) esto es igual al metodo de abajo  <--esto es JPQL 
 	public Iterable<Carrera> findCarrerasByNombreContainsIgnoreCase(String nombre);  //Esto es JPQL con metodos 
-	
-	
-	//@Query ("select c from Carrera c where c.cantidadAnios > ?1) Significa que cantidad sea mayor al dato que envie, cantidadAnios debe ser escrito igual que la clase Carrera
 	public Iterable<Carrera> findCarrerasByCantidadAniosAfter(Integer cantidadAnios);
 	
-	//Aqui va el Query
 	@Query ("select c from Carrera c join fetch c.profesores pr where pr.nombre like %?1% and pr.apellido like %?2%")
 	public Iterable<Carrera> buscarCarrerasPorProfesorNombreYApellido(String nombre, String apellido);
 
